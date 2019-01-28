fc24422a3a7a2e90ac5ee78c7babe25b85c0291a
dylankbuckley <dbuckley14@icloud.com>
2019-01-28 21:11:52 +0000

    Hello World

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    // "fmt"
     "encoding/json"
     "github.com/gorilla/mux"
     "log"
@@ -8,67 +9,20 @@ import (
     "os"
 )
 
-// The person Type (more like an object)
-type Person struct {
-    ID        string   `json:"id,omitempty"`
-    Firstname string   `json:"firstname,omitempty"`
-    Lastname  string   `json:"lastname,omitempty"`
-    Address   *Address `json:"address,omitempty"`
-}
-type Address struct {
-    City  string `json:"city,omitempty"`
-    State string `json:"state,omitempty"`
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+    json.NewEncoder(w).Encode("Hello World, My Name is Dylan. This is my first GO app")
 }
 
-var people []Person
-
-// Display all from the people var
-func GetPeople(w http.ResponseWriter, r *http.Request) {
-    json.NewEncoder(w).Encode(people)
-}
-
-// Display a single data
-func GetPerson(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    for _, item := range people {
-        if item.ID == params["id"] {
-            json.NewEncoder(w).Encode(item)
-            return
-        }
-    }
-    json.NewEncoder(w).Encode(&Person{})
-}
-
-// create a new item
-func CreatePerson(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    var person Person
-    _ = json.NewDecoder(r.Body).Decode(&person)
-    person.ID = params["id"]
-    people = append(people, person)
-    json.NewEncoder(w).Encode(people)
-}
-
-// Delete an item
-func DeletePerson(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    for index, item := range people {
-        if item.ID == params["id"] {
-            people = append(people[:index], people[index+1:]...)
-            break
-        }
-        json.NewEncoder(w).Encode(people)
-    }
-}
-
-// main function to boot up everything
 func main() {
     router := mux.NewRouter()
-    people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-    people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
-    router.HandleFunc("/people", GetPeople).Methods("GET")
-    router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
-    router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
-    router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
-    log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+    router.HandleFunc("/", HelloWorld).Methods("GET")
+    log.Fatal(http.ListenAndServe(GetRuntimePort(), router))
+}
+
+func GetRuntimePort() string {
+  if len(os.Getenv("PORT")) > 0 {
+    return ":"+os.Getenv("PORT")
+  } else {
+    return ":8080"
+  }
 }
